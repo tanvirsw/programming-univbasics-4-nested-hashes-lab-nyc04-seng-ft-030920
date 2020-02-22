515570d7a5d4aa8bb89d2693b045bcadc69e9877
Tanvir Walia <tarywalia@gmail.com>
2020-02-22 16:35:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,34 +62,14 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-	programmer_hash =
-	{
-			:grace_hopper => {
-				:known_for => "COBOL",
-				:languages => ["COBOL", "FORTRAN"]
-			},
-			:alan_kay => {
-				:known_for => "Object Orientation",
-				:languages => ["Smalltalk", "LISP"]
-			},
-			:dennis_ritchie => {
-				:known_for => "Unix",
-				:languages => ["C"]
-			}
-			:yukihiro_matsumoto => {
-				 :known_for => "Ruby",
-				 :languages => ["LISP","C"]
-	 }
-}
-end
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
- 		{
+  programmer_hash =
+    {
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -104,7 +84,10 @@ end
         }
      }
 
-
+		 programmer_hash[:yukihiro_matsumoto] = {}
+		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+		 programmer_hash
 end
 
 def changing_alan
